03a4a47450ea0be367af05833f5acb6cb663c34f
Lucas <lucas@eluke.nl>
2024-07-23 21:43:48 +0200

    modified for 8-bit data output, at a slower rate (pio clock division), todo: test if it indeed functions as thought

--- spielerei.c
+++ spielerei.c
@@ -4,7 +4,8 @@
 #include "pico/binary_info.h"
 #include "spielerei.pio.h"
 
-const uint LED_PIN = 16;
+const uint LED_PIN = 16; // now uses pin 16,17,18,19,20,21,22,23,24 to output 8 bit parallel data
+
 int main()
 {
     bi_decl(bi_program_description("This is a test binary."));
@@ -16,14 +17,21 @@ int main()
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &spielerei_program);
     uint sm = pio_claim_unused_sm(pio, true);
-   // spielerei_program_init(pio, sm, offset, LED_PIN); //do not start the pio yet. First blink using software below:
-    
+
+    spielerei_program_init(pio, sm, offset, LED_PIN); // TODO: should modify the init fucntion to make it clear it uses multiple pins for parallel data
+
     while (1)
     {
+        pio_sm_put_blocking(pio,sm,0x5555); // for now, put a dataword this way. Later, figure out DMA to write out a display buffer
+        pio_sm_put_blocking(pio,sm,0xAAAA);
+        sleep_ms(250);
+
+/*
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
         gpio_put(LED_PIN, 1);
         puts("Hello World\n");
         sleep_ms(1000);
+  */
     }
 }
