b3ff27acafe76098af09613280a521a8ccb74d7d
Lucas <10280695+Lukelectro@users.noreply.github.com>
2024-07-30 22:57:12 +0200

    initial setup of sideset pins and 'set' pins needs to be done with the sm_config_set_...()-functions and not the pio_sm_set_...()-functions, it seems

--- spielerei.c
+++ spielerei.c
@@ -11,26 +11,18 @@ int main()
     bi_decl(bi_program_description("This is a test binary."));
     bi_decl(bi_1pin_with_name(LED_PIN, "LED op breadboard"));
     stdio_init_all();
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
     
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &spielerei_program);
     uint sm = pio_claim_unused_sm(pio, true);
 
     spielerei_program_init(pio, sm, offset, LED_PIN); // TODO: should modify the init fucntion to make it clear it uses multiple pins for parallel data
-
+    
     while (1)
     {
         uint counter;
         counter++;
         pio_sm_put_blocking(pio,sm,counter); // for now, put a dataword this way. Later, figure out DMA to write out a display buffer
-        /*
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
-        sleep_ms(1000);
-  */
     }
+
 }
