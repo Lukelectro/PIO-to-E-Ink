369a87e17918f83d06b2fe24ee206bdd5ff105d4
Lucas <10280695+Lukelectro@users.noreply.github.com>
2024-08-09 22:13:24 +0200

    e-ink spielerei. Surprisingly this builds. It has not been tested yet

--- spielerei.c
+++ spielerei.c
@@ -3,6 +3,7 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include "spielerei.pio.h"
+#include "chargepump.pio.h"
 
 const uint LED_PIN = 15; // now uses pin 15,16,17,18,19,20,21,22 to output 8 bit parallel data
 
@@ -13,16 +14,20 @@ int main()
     stdio_init_all();
     
     PIO pio = pio0;
-    uint offset = pio_add_program(pio, &spielerei_program);
-    uint sm = pio_claim_unused_sm(pio, true);
+    uint offset_spiel = pio_add_program(pio, &spielerei_program);
+    uint sm_spiel = pio_claim_unused_sm(pio, true);
+    uint offset_ch = pio_add_program(pio, &chargepump_program);
+    uint sm_ch = pio_claim_unused_sm(pio, true);
 
-    spielerei_program_init(pio, sm, offset, LED_PIN); // TODO: should modify the init fucntion to make it clear it uses multiple pins for parallel data
+    spielerei_program_init(pio, sm_spiel, offset_spiel, LED_PIN); // TODO: should modify the init fucntion to make it clear it uses multiple pins for parallel data
+    chargepump_program_init_and_start(pio,sm_ch,offset_ch,12,50000);// 50 kHz charge pump waveforms on pins 12 and 13
+
     
     while (1)
     {
         uint counter;
         counter++;
-        pio_sm_put_blocking(pio,sm,counter); // for now, put a dataword this way. Later, figure out DMA to write out a display buffer
+        pio_sm_put_blocking(pio,sm_spiel,counter); // for now, put a dataword this way. Later, figure out DMA to write out a display buffer
     }
 
 }
