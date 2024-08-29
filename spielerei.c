80832c6a9b12a228a69b25739e3c740c4e7a9482
Lucas <10280695+Lukelectro@users.noreply.github.com>
2024-08-29 23:01:40 +0200

    Does not work, triggers a hardfault

--- spielerei.c
+++ spielerei.c
@@ -5,6 +5,7 @@
 #include "pico/binary_info.h"
 #include "spielerei.pio.h"
 #include "chargepump.pio.h"
+#include "ED060SC4_row_write.pio.h"
 #include "epd.h"
 
 const uint LED_PIN = 14; // now uses pin 15,16,17,18,19,20,21,22 to output 8 bit parallel data
@@ -18,12 +19,10 @@ void dispdata_init()
         for (uint i = 0; i < DISPDATASIZE; i++)
         {
             if (i<DISPDATASIZE/2)
-                //dispdata[i + j] = 0x55555555; // testen met 0x55555555 en 0xAAAAAAAA om zwart en wit op display te krijgen, 0b01 en 0b10 doen iets maar 11 en 00 zijn 'doe niks'.
-                dispdata[i] = 0x55EEFFC0; // maar om te kijken of 8 bits gebruikt en 24 weggegooit, of dat het per 8 bits gebruikt wordt, even andere testdata (It gets send small endian, so reversed for readability)
-            else
-                //dispdata[i + j] = 0xAAAAAAAA;
-                dispdata[i] = 0xEFBEADDE;
-        }
+                dispdata[i] = 0x55555555; // testen met 0x55555555 en 0xAAAAAAAA om zwart en wit op display te krijgen, 0b01 en 0b10 doen iets maar 11 en 00 zijn 'doe niks'.
+                else
+                dispdata[i] = 0xAAAAAAAA;
+       }
     dispdata[0]=0; //initial row skip
 }
 
@@ -33,37 +32,35 @@ int main()
     bi_decl(bi_1pin_with_name(LED_PIN, "LED op breadboard"));
     stdio_init_all();
     
-    //pio_sm_claim(pio0,0); // reserve PIO0, State machine 0 for the DMA transfer
+    pio_sm_claim(pio0,0); // reserve PIO0, State machine 0 for the DMA transfer
 
-
-    //PIO pio = pio0;
-    //uint offset_spiel = pio_add_program(pio, &spielerei_program);
-    //uint sm_spiel = pio_claim_unused_sm(pio, true);
+    PIO pio = pio0;
+    uint offset_dmarw = pio_add_program(pio, &rowwrite_program);
+    uint sm_dmarw = pio_claim_unused_sm(pio, true);
     //uint offset_ch = pio_add_program(pio, &chargepump_program);
     //uint sm_ch = pio_claim_unused_sm(pio, true);
 
     //chargepump_program_init_and_start(pio,sm_ch,offset_ch,12,50000);// 50 kHz charge pump waveforms on pins 12 and 13
+    int* pdmach;
+    rowwrite_program_init(pio,sm_dmarw,offset_dmarw,14,10,2);
+    DMA_setup_eink_rowwrite(&dispdata, DISPDATASIZE, pio, sm_dmarw, pdmach);
+
+     dispdata_init();
 
     EPD_Init();
     EPD_Power_On();
-    EPD_Clear();
+    EPD_Clear(); // dit nog in software
 
-    for(int j=0;j<600;j+=160){
-    for(int i =0;i<800;i+=25){
-    EPD_String_24(j+25,i,"Hello World!",1);
-    }
-    }
-    EPD_String_24(400,350,"www.eLuke.nl",1);
-  
+    start_a_dma_eink_rowwrite(pdmach,dispdata,pio);
 
-    EPD_DispScr(0,600);
+
 
     EPD_Power_Off();
 
 // spielerij pio program is on the same pins, so init/start it only AFTER the other test with the display
     //spielerei_program_init(pio, sm_spiel, offset_spiel, LED_PIN); // TODO: should modify the init fucntion to make it clear it uses multiple pins for parallel data
     
-    dispdata_init();
+   
     //int dmach = dma_claim_unused_channel(true);
     //dma_channel_config eink_dma_ch_config = dma_channel_get_default_config(dmach);
     //default config has read increment and write to fixed adres, 32 bits wide, which is indeed what's needed here
