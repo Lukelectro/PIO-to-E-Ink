fa3653177be5f0daf8002333a0fad279399875ae
Lucas <10280695+Lukelectro@users.noreply.github.com>
2024-09-16 23:06:00 +0200

    Tried to add mcufont, compiled on Linux, but run ito multiple definition error, possibly something wrong with my cmake file as I have not realy worked with cmake before.

--- spielerei.c
+++ spielerei.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/dma.h"
 #include "pico/binary_info.h"
 #include "ED060SC7_refresh.pio.h"
 #include "aimonen/gdisp_lld.h"
+#include "mcufont/fonts/fonts.h"
+#include "mcufont/decoder/mcufont.h"
 
     union screenbuffer displaydata; // TODO: make it local to gdisp_lld.c and include all the DMA-related config there too...
+
+static void pixel_callback(int16_t x, int16_t y, uint8_t count, uint8_t alpha, void *state)
+    {
+    while (count--)
+        {
+            gdisp_lld_draw_pixel(x,y,0);/* your code goes here, ex: drawPixel(x, y, alpha, color::black); */
+            x++;
+        }
+    }
+
+static uint8_t char_callback(int16_t x0, int16_t y0, mf_char character, void *state)
+    {
+        return mf_render_character(&mf_rlefont_DejaVuSans12.font, x0, y0, character, &pixel_callback, state);
+    }
+
 
 int main()
 {
@@ -69,6 +87,13 @@ int main()
 
     epd_refresh_program_init(pio,sm_dmarw,offset_dmarw,14,10,2); // now let PIO snatch the pins
 
+    mf_render_aligned(
+       &mf_rlefont_DejaVuSans12.font,
+       50, 50,
+       MF_ALIGN_LEFT,
+       "e-ink.eluke.nl test with e-ink driver that does DMA to PIO", strlen("e-ink.eluke.nl test with e-ink driver that does DMA to PIO"),
+       &char_callback, NULL);
+
 
 /* write the config and DO NOT YET start the transfer */
    dma_channel_configure(
@@ -80,24 +105,29 @@ int main()
         false // true to start imeadeately, false to start later
     );
 
-    for(int grayframe=0;grayframe<4;grayframe++){
-    if(!dma_channel_is_busy(dmach))
-        {
-            // once DMA is no longer busy, load new data and restart transfer           
-            dma_channel_set_read_addr(dmach, &displaydata.sb_words[0][0], true); // re-set read adress and restart transfer
-        }
 
+   for (int grayframe = 0; grayframe < 4; grayframe++)
+   {
+       if (!dma_channel_is_busy(dmach))
+       {
+           // once DMA is no longer busy, load new data and restart transfer
+           dma_channel_set_read_addr(dmach, &displaydata.sb_words[0][0], true); // re-set read adress and restart transfer
+       }
 
-        for(uint y=300;y<400;y++){
-        for(uint x=100;x<300+grayframe*100;x++){
-        gdisp_lld_draw_pixel(x,y,0); // expanding black block (resulting in various shades of gray)
-        }
-        }
- 
-    while(dma_channel_is_busy(dmach)){}; // wait untill DMA is done before powering off
-    busy_wait_us(350); // test with a forced delay in between rewrites
-    //might it be a power supply issue?
-    //busy_wait_ms(500); // lets try a reeeal ssllllloowwwwww delay to see if it is the power suply
+       for (uint y = 300; y < 400; y++)
+       {
+           for (uint x = 100; x < 300 + grayframe * 100; x++)
+           {
+               gdisp_lld_draw_pixel(x, y, 0); // expanding black block (resulting in various shades of gray)
+           }
+       }
+
+       while (dma_channel_is_busy(dmach))
+       {
+       };                 // wait untill DMA is done before powering off
+       busy_wait_us(350); // test with a forced delay in between rewrites
+       // might it be a power supply issue?
+       // busy_wait_ms(500); // lets try a reeeal ssllllloowwwwww delay to see if it is the power suply
     }
     busy_wait_ms(5000); // then wait a bit longer just for the bit in FIFO to be writen to the display. 
     //(TODO: in practice CPU should be doing something usefull and/or the busy/done signal should be used to know when to powerdown the eink)
