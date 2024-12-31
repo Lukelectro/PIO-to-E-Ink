b5a092e27c169958d644d6475354ca04a501d5f4
Lucas <10280695+Lukelectro@users.noreply.github.com>
2024-12-31 21:09:46 +0100

    proper demo screen layout

--- spielerei.c
+++ spielerei.c
@@ -56,9 +56,9 @@ int main()
            dma_channel_set_read_addr(dmach, &displaydata.sb_words[0][0], true); // re-set read adress and restart transfer
        }
 
-       for (uint y = 300; y < 400; y++)
+       for (uint y = 500; y < 600; y++)
        {
-           for (uint x = 100; x < 200 + grayframe * 100; x++)
+           for (uint x = 10; x < 110 + grayframe * 100; x++)
            {
                gdisp_lld_draw_pixel(x, y, 0); // expanding black block (resulting in various shades of gray)
            }
@@ -66,36 +66,32 @@ int main()
 
        while (dma_channel_is_busy(dmach))
        {
-       };                 // wait untill DMA is done before powering off
-       busy_wait_us(350); // test with a forced delay in between rewrites
-       // might it be a power supply issue? TODO: Why is this delay needed for a proper display?
+       };                 // wait untill DMA is done before changing data and starting next write
+       busy_wait_us(350); // TODO: Why is this delay needed for a proper display?
     }
 
 
     /* prepare text data to write to display */
-    clear_screenbuffer(NOCHANGE00); // prefill screen buffer with "no change" data. Could fill with background color (black or white) but then single pixels lines do not show properly!
+    clear_screenbuffer(NOCHANGE00); // prefill screen buffer with "no change" data.
     eink_set_font("DejaVuSerif32");
-    text_to_eink(0, 0, "e-ink.eluke.nl",ROT_0);
+    text_to_eink(10, 0, "E-ink.Eluke.nl",ROT_0);
+    text_to_eink(10, 0, "E-ink.Eluke.nl",ROT_180);
     eink_set_font("DejaVuSerif16");
-    text_to_eink(100, 32, "Demo with e-ink driven by DMA to PIO",ROT_0);
-/*
-    //text rotation not relevant for demo
-    text_to_eink(10,10, "Downside up", ROT_180);   
-    text_to_eink(10,10, "90 degree rotated text", ROT_90); 
-    text_to_eink(30,10, "Or 90 degrees the other way", ROT_270);
-*/
+    text_to_eink(10, 35, "Demo with e-ink driven by DMA to PIO",ROT_0);
+    text_to_eink(10, 35, "Demo with e-ink driven by DMA to PIO",ROT_180);
+    text_to_eink(10, 60, "Downside up, Upside down",ROT_180);
+    text_to_eink(250,380, "Hello World!", ROT_90); 
+    text_to_eink(250,380, "Yellow Bird!", ROT_270);
+    eink_set_font("fixed_10x20");
+    text_to_eink(10, 480, "Grayscale withouth dithering.",ROT_0);
 
-    eink_set_font("fixed_7x14"); 
-    text_to_eink(0,50, "Various fonts", ROT_0);
-    for(uint i = 0; i<500; i++) {
-    gdisp_lld_draw_pixel(i, 70, BLACK);  // 1px lijntje dat hopelijk niet verdwijnt 
-    gdisp_lld_draw_pixel(780, i, BLACK); // 1px lijntje dat hopelijk niet verdwijnt
-     gdisp_lld_draw_pixel(i, 75, WHITE); // 1px lijntje dat hopelijk niet verdwijnt 
-    gdisp_lld_draw_pixel(750, i, WHITE); // 1px lijntje dat hopelijk niet verdwijnt
+
+    for(uint i = 250; i<350; i++) {
+    gdisp_lld_draw_pixel(400, i, BLACK);  // 1px lijntje
     }
 
 /* Write text. Because background now is "no change" the grayscale block stays on the display. */
-       for (int grayframe = 0; grayframe < 4; grayframe++)
+       for (int grayframe = 0; grayframe < 3; grayframe++) /* text is written multiple times too, so it can also be displayed in shades. */
    {
        if (!dma_channel_is_busy(dmach))
        {
@@ -106,8 +102,7 @@ int main()
        while (dma_channel_is_busy(dmach))
        {
        };               
-       busy_wait_ms(1); // test with a forced delay in between rewrites
-       // might it be a power supply issue?
+       busy_wait_us(350); 
     }
 
     
